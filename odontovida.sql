15b3ae5727eece6479725a4cbcc2ccb928156fdc
Samambaia27 <emillygabrielly.dasilva27@gmail.com>
2024-09-09 16:45:37 -0300

    Update odontovida.sql

--- odontovida.sql
+++ odontovida.sql
@@ -35,3 +35,32 @@ FOREIGN KEY (paciente_id) REFERENCES Paciente(paciente_id),
 FOREIGN KEY (dentista_id) REFERENCES Dentista (dentista_id) 
 );
 
+CREATE TABLE Tratamento (
+	tratamento_id INT auto_increment PRIMARY KEY,
+    consulta_id INT,
+    descricao TEXT,
+    data_inicio DATE,
+    data_fim DATE,
+    FOREIGN KEY (consulta_id) REFERENCES Consulta(consulta_id)
+);
+CREATE TABLE Procedimento (
+	procedimento_id INT auto_increment PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    custo DECIMAL(10, 2) NOT NULL
+);
+CREATE TABLE Tratamento_Procedimento (
+	tratamento_id INT,
+    procedimento_id INT,
+    quantidade INT DEFAULT 1,
+    primary key (tratamento_id, procedimento_id),
+    foreign key (tratamento_id) references Tratamento(tratamento_id),
+    foreign key (procedimento_id) references Procedimento(procedimento_id)
+    );
+CREATE TABLE Pagamento (
+	pagamento_id INT AUTO_INCREMENT PRIMARY KEY,
+    tratamento_id INT,
+    valor DECIMAL(10,2) NOT NULL,
+    data_pagamento DATE,
+    foreign key (tratamento_id) references Tratamento(tratamento_id)
+);
+
