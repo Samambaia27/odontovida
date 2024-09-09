19d540020257853afc3c9bc3297e917a4f585f9d
Samambaia27 <emillygabrielly.dasilva27@gmail.com>
2024-09-09 16:44:34 -0300

    Update odontovida.sql

--- odontovida.sql
+++ odontovida.sql
@@ -10,3 +10,28 @@ create database OdontoVida;
 -- 2. Acessando  Arquivo/Banco de Dados 
 use OdontoVida;
 
+-- 3 criando Tabelas
+CREATE TABLE Paciente (
+paciente_id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+data_nascimento DATE NOT NULL,
+telefone VARCHAR(20),
+endereco TEXT, 
+email VARCHAR(100)
+);
+CREATE TABLE Dentista (
+dentista_id INT AUTO_INCREMENT PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+especialidade VARCHAR(50),
+telefone VARCHAR(20),
+email VARCHAR(100)
+);
+CREATE TABLE Consulta (
+consulta_id INT AUTO_INCREMENT PRIMARY KEY,
+paciente_id INT,
+Dentista_id INT,
+data_consulta DATETIME,
+FOREIGN KEY (paciente_id) REFERENCES Paciente(paciente_id),
+FOREIGN KEY (dentista_id) REFERENCES Dentista (dentista_id) 
+);
+
