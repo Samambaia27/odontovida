60269f28b2d5aa3dc6fb01c6ffb215e0bc34987f
Samambaia27 <emillygabrielly.dasilva27@gmail.com>
2024-09-09 16:43:01 -0300

    Update odontovida.sql

--- odontovida.sql
+++ odontovida.sql
@@ -1,23 +1,12 @@
-INSERT INTO Consulta (paciente_id, dentista_id, data_consulta) VALUES
-(1, 1, '2024-09-10 10:00:00'),
-(2, 2, '2024-09-11 11:00:00'),
-(3, 3, '2024-09-12 09:00:00'),
-(4, 4, '2024-09-13 14:00:00'),
-(5, 5, '2024-09-14 15:00:00'),
-(6, 6, '2024-09-15 08:00:00'),
-(7, 7, '2024-09-16 13:00:00'),
-(8, 8, '2024-09-17 16:00:00'),
-(9, 9, '2024-09-18 12:00:00'),
-(10, 10, '2024-09-19 17:00:00');
-INSERT INTO Tratamento (consulta_id, descricao, data_inicio, data_fim) VALUES
-(1, 'Limpeza e Poliamento', '2024-09-10', '2024-09-10'),
-(2, 'Tratamento de Canal', '2024-09-11', '2024-09-25'),]
-(3, 'Aplicação de Flúor', '2024-09-12', '2024-09-12'),
-(4,  'Extração de Dente', '2024-09-13', '2024-09-13'),
-(5, 'Colocação de Aparelho', '2024-09-14', '2024-12-14'),
-(6, 'Implante Dentário', '2024-09-15', '2024-10-15'),
-(7, 'Clareamento Dentário', '2024-09-16', '2024-09-16'),
-(8, 'Proótese Paecial', '2024-09-17', '2024-10-17'),
-(9, 'Tratamento de Gengiva', '2024-09-18', '2024-09-25'),
-(10, 'Reparo de Prótese', '2024-09-19', '2024-09-19');
+/*
+SENAC DF - CEP Jessé Freire 
+2024.07.276 - Aprendizagem Profissional de Qualificação em Desenvolvimento de Softwares 
+Prof° Hudson Neves 
+Projeto Final - Implementação de Banco de Dados Relacional com MySQL
+Projeto - Clinica Odontológica OdontoVida */
+
+-- 1. Criação do Arquivo/Banco de Dados
+create database OdontoVida;
+-- 2. Acessando  Arquivo/Banco de Dados 
+use OdontoVida;
 
